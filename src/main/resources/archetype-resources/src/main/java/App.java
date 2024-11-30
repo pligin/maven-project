53f5543c4ea357b0c6139acb1e412d590627b1e6
Mr. Pligin <i@psweb.ru>
2024-11-30 12:47:48 +0300

    Add profiles and logging

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -1,4 +1,6 @@
 package $ru.psweb;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Hello world!
@@ -6,8 +8,24 @@ package $ru.psweb;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    static final Logger rootLogger = LogManager.getRootLogger();
+
+    public static void main(String[] args) {
+
+        rootLogger.info("info message");
+
+        // debug
+        if (rootLogger.isDebugEnabled()) {
+            rootLogger.debug("In debug message");
+        }
+        try {
+            throw new NullPointerException("Null!!!");
+        } catch (NullPointerException ex) {
+            rootLogger.error("error message: " + ex.getMessage());
+            rootLogger.fatal("fatal error message: " + ex.getMessage());
+        }
+
         System.out.println( "Hello World!" );
     }
+
 }
